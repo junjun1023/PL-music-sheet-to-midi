62c23b1b393f186a9bc35eb09199df1e17cb4c91
junjun1023 <junjun1023@gmail.com>
2019-06-01 21:01:34 +0800

    getting lines done

--- getting_lines.py
+++ getting_lines.py
@@ -11,10 +11,10 @@ def preprocess_image(image):
     if config.VERBOSE:
         print("Preprocessing image.")
     gray = image.copy()
-    _, thresholded = cv2.threshold(gray, config.THRESHOLD_MIN, config.THRESHOLD_MAX, cv2.THRESH_BINARY)
-    element = np.ones((3, 3))
-    thresholded = cv2.erode(thresholded, element)
-    edges = cv2.Canny(thresholded, 10, 100, apertureSize=3)
+    _, thresholded = cv2.threshold(gray, config.THRESHOLD_MIN, config.THRESHOLD_MAX, cv2.THRESH_BINARY) # 影像二值化, 固定閾值
+    element = np.ones((3, 3)) # 3*3 的矩陣=1
+    thresholded = cv2.erode(thresholded, element)   # 侵蝕/磨平，去除掉凸出來的
+    edges = cv2.Canny(thresholded, 10, 100, apertureSize=3) # edge detection
     return edges, thresholded
 
 """
@@ -24,7 +24,7 @@ def preprocess_image(image):
     :param nlines: How many lines we want to process.
     :return: A list of horizontal lines and an image with lines drawn on it.
 """
-def detect_lines(hough, image, nlines):
+def detect_lines(hough, image, nlines): 
     if config.VERBOSE:
         print("Detecting lines.")
     all_lines = set()
@@ -51,9 +51,63 @@ def detect_lines(hough, image, nlines):
         diff = y2 - y1
         if abs(diff) < config.LINES_ENDPOINTS_DIFFERENCE:
             all_lines.add(int((start[1] + end[1]) / 2))
-            cv2.line(lines_image_color, start, end, (0, 0, 255), 2)
+            cv2.line(lines_image_color, start, end, (0, 0, 255), 2) # draw lines
 
     if config.SAVING_IMAGES_STEPS:
-        cv2.imwrite("output/5lines.png", lines_image_color)
+        cv2.imwrite("output/lines.png", lines_image_color)
 
     return all_lines, lines_image_color
+
+"""
+    Detects staffs in the image.
+    :param all_lines: all lines detected in the image
+    :return: list of tuples with beginnings and ends of staffs detected in the image
+"""
+def detect_staffs(all_lines):
+    if config.VERBOSE:
+        print("Detecting staffs.")
+    staffs = []
+    lines = []
+    all_lines = sorted(all_lines)
+    for current_line in all_lines:
+        # If current line is far away from last detected line
+        if lines and abs(lines[-1] - current_line) > config.LINES_DISTANCE_THRESHOLD:
+            if len(lines) >= 5:
+                # Consider it the start of the next staff.
+                # If <5 - not enough lines detected. Probably an anomaly - reject.
+                staffs.append((lines[0], lines[-1]))
+            lines.clear()
+        lines.append(current_line)
+
+    # Process the last line
+    if len(lines) >= 5:
+        if abs(lines[-2] - lines[-1]) <= config.LINES_DISTANCE_THRESHOLD:
+            staffs.append((lines[0], lines[-1]))
+    return staffs
+
+"""
+    Draws staffs on the image.
+    :param image: image to write staffs onto
+    :param staffs: list of Staff to draw
+"""
+def draw_staffs(image, staffs):
+    # Draw the staffs
+    width = image.shape[0]
+    for staff in staffs:
+        cv2.line(image, (0, staff[0]), (width, staff[0]), (0, 255, 255), 2)
+        cv2.line(image, (0, staff[1]), (width, staff[1]), (0, 255, 255), 2)
+    if config.SAVING_IMAGES_STEPS:
+        cv2.imwrite("output/staffs.png", image)
+
+"""
+    Returns a list of Staff
+    :param image: image to get staffs from
+    :return: list of Staff
+"""
+def get_staffs(image):
+    processed_image, thresholded = preprocess_image(image)
+    hough = cv2.HoughLines(processed_image, 1, np.pi / 150, 200)    # detecting line
+    all_lines, lines_image_color = detect_lines(hough, thresholded, 80)
+    staffs = detect_staffs(all_lines)
+    draw_staffs(lines_image_color, staffs)
+    return [Staff(staff[0], staff[1]) for staff in staffs]
